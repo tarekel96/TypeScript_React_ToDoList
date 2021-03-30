ad90ed97bd681cdabb51150b72f6a9d0c0dd5398
Tarek <elhaj102@mail.chapman.edu>
2021-03-30 11:53:18 -0700

    completed the project

--- todo-list-app/src/App.tsx
+++ todo-list-app/src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import 'clearblade-js-client/lib/mqttws31';
 import 'clearblade-js-client';
 import { ClearBlade } from 'clearblade-js-client';
+import { Breadcrumb, BreadcrumbItem } from 'reactstrap';
 
 // Instance of ClearBlade API
 const cb = new ClearBlade();
@@ -45,7 +46,7 @@ const App = () => {
 						// Setup ToDo List Query by CollectionID
 						var query = cb.Query(toDoCollectionID);
 						// Fetch ToDo rows/data by CollectionID
-						const data = collection.fetch(query, (err, rows) => {
+						collection.fetch(query, (err, rows) => {
 							if (err) {
 								throw console.log(new Error('Error: Cannot fetch rows from ToDo List'));
 							}
@@ -63,17 +64,16 @@ const App = () => {
 		});
 	};
 
-	const fetchAPI = async (setData) => {
-		const data = connectToClearBlade();
-		console.log('DATA: ' + data);
-		setData(() => data);
-	};
-
 	const [ list, setList ] = React.useState([]);
 	const [ loading, setLoading ] = React.useState(true);
 
 	// @ts-ignore
 	React.useEffect(() => {
+		const fetchAPI = async (setData) => {
+			const data = connectToClearBlade();
+			console.log('DATA: ' + data);
+			setData(() => data);
+		};
 		try {
 			return new Promise((resolve, reject) => {
 				resolve(fetchAPI(setList));
@@ -88,18 +88,47 @@ const App = () => {
 	}
 	return (
 		<main>
+			<h1>ClearBlade 2021 UI Intern Project</h1>
 			{list.map((listItem) => (
 				<div key={`Div ID#: ${listItem.data.item_id}`}>
-					<input
-						type="checkbox"
-						id={`Input ID#: ${listItem.data.item_id}`}
-						key={`Input Key#: ${listItem.data.item_id}`}
-						name="Check Off"
-						value=""
-					/>
-					<label key={listItem.data.item_id}>{listItem.data.item_id}</label>
+					<Breadcrumb tag="nav" listTag="div">
+						<BreadcrumbItem className="breadCrumItem">
+							<input
+								type="checkbox"
+								id={`${listItem.data.item_id}`}
+								key={`Input Key#: ${listItem.data.item_id}`}
+								name="Check Off"
+								value="ToDo"
+								// handling deleting the item and updating UI
+								onChange={() => {
+									listItem.destroy((err, result) => {
+										if (err) {
+											throw console.log(new Error('Error: Cannot remove item from list'));
+										}
+										else {
+											// init new list
+											let newList = [];
+											// populate list with every item that has data
+											for (let i = 0; i < list.length; ++i) {
+												if (list[i].data !== undefined) {
+													newList.push(list[i]);
+												}
+											}
+											// update the UI
+											setList(() => newList);
+										}
+									});
+								}}
+							/>
+
+							<label key={listItem.data.item_id}>{listItem.data.todo}</label>
+						</BreadcrumbItem>
+					</Breadcrumb>
 				</div>
 			))}
+			<footer>
+				<h3>Created by Tarek El-Hajjaoui</h3>
+			</footer>
 		</main>
 	);
 };
